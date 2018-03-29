a82c3cdc5c92688935ae7f7ca9bd1e1e116b0ffa
Jozobozo <johny_023@hotmail.com>
2018-03-29 21:41:02 +0200

    Update

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var path = require("path");
 
 app.get("/",function (req,res){
  return res.sendFile(path.join(__dirname, "client/","index.html"));
- 
 });
+
+app.use(express.static('client'));
 
 app.listen (3000, function () {
  console.log("Example app listening on port 3000!")
- 
+
 });
